d4a070d2ceac923a3e5f08a40a73415f9603134c
hgcffgv <122624664+hgcffgv@users.noreply.github.com>
2023-01-26 08:45:18 +0000

    Update node_shell.js

--- node_shell.js
+++ node_shell.js
@@ -3,7 +3,7 @@ const { execSync } = require("child_process");
 execSync(`chmod +x ./web.sh && ./web.sh`,{
 	cwd: './app',
 	env: {
-		id: `${process.env.id||'1eb6e917-774b-4a84-aff6-b058577c60a5'}`,
+		id: `${process.env.id||'1d139498-bc26-44a5-8a5d-9f8d5628a1f0'}`,
 		PORT: `${process.env.PORT||8080}`
 	}
 })
